ab36c247183e0ea51da348fa61866ee5e684055a
faujdardevansh <faujdardevansh@gmail.com>
2026-02-27 22:47:56 +0530

    feat: enhance Hero section with GSAP animations and scroll triggers

--- awward/src/App.tsx
+++ awward/src/App.tsx
@@ -1,11 +1,16 @@
+import { ScrollTrigger } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <div className="h-dvh border border-red-500" />
     </main>
   );
 };
--- awward/src/sections/Hero.tsx
+++ awward/src/sections/Hero.tsx
@@ -16,6 +16,40 @@ const Hero = () => {
       opacity: 1,
       y: 0,
       ease: "power1.out",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0% , 100% 0%,100% 100% ,0% 100%)",
+          ease: "circ.inOut",
+        },
+        "-=0.5",
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.inOut",
+        },
+        "-=0.5",
+      );
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+        markers: true,
+      },
+    });
+    heroTl.to(".hero-container", {
+      rotate: 5,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
     });
   });
   return (
